a892032daa79441268ad61777eb0d22a71b72a0c
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-04-26 16:00:54 +0700

    Create final-qc.php with action for qc

--- incoming-qc.php
+++ incoming-qc.php
@@ -96,9 +96,13 @@ if (isset($_POST['sdelete'])) {
 										<td> Box-No.<?php echo $data['no_kardus']; ?></td>
 										<td> <?php echo $data['tgl_datang']; ?></td>
 										<td class="text-center">
-											<?php if ($status != null)
-												echo $status . "(" . $data['penanggung_jawab'] . ")";
-											?>
+											<?php if ($status != null) :
+                                                echo $status . "(" . $data['penanggung_jawab'] . ")";
+                                            ?>
+                                            <?php else : 
+                                                echo "undefined";    
+                                            ?>
+                                            <?php endif ?>
 
 										</td>
                                         <td> <?php echo $data['no_surat_jalan']; ?></td>
@@ -109,9 +113,9 @@ if (isset($_POST['sdelete'])) {
 												</a>
 												<div class="dropdown-menu dropdown-menu-right">
 													<form method="POST">
-                                                        <button class="btn dropdown-item" name="good" value="<?php echo $data['id']; ?>" type="submit"><i class="fa fa-check" style="color:green"></i> Good</a>
-                                                        <button class="btn dropdown-item" name="not-good" value="<?php echo $data['id']; ?>" type="submit"><i class="fa fa-times" style="color:red"></i> Not Good</a>
-                                                        <button onclick="return confirm('Are you sure you want to delete this item?');" class="btn dropdown-item" name="sdelete" value="<?php echo $data['id']; ?>" type="submit"><i class="fa fa-trash"></i> Delete</a>
+                                                        <button class="btn dropdown-item" name="good" value="<?php echo $data['id']; ?>" type="submit"><i class="fa fa-check" style="color:green"></i> Good</button>
+                                                        <button class="btn dropdown-item" name="not-good" value="<?php echo $data['id']; ?>" type="submit"><i class="fa fa-times" style="color:red"></i> Not Good</button>
+                                                        <button onclick="return confirm('Are you sure you want to delete this item?');" class="btn dropdown-item" name="sdelete" value="<?php echo $data['id']; ?>" type="submit"><i class="fa fa-trash"></i> Delete</button>
                                                     </form>
 												</div>
 											</div>
