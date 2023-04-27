38cdd0c938fff079aabc2bb0f9ca3bada96fe26c
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-04-27 09:44:04 +0700

    Added label penanggung_jawab

--- serial-number.php
+++ serial-number.php
@@ -133,7 +133,7 @@
 										?></td>										
 										<td class="text-center"> 
 											<?php if ($status != null) :
-												echo $status;
+												echo $status . "(" . $data['penanggung_jawab'] . ")";
 											?>
 											<?php else : 
                                                     echo "undefined";    
